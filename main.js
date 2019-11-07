72a1af04d054b02da7ddfe88caed74fea3734a98
Michael Ponti <mike.ponti@honeywell.com>
2019-11-07 10:26:50 -0800

    task: fix up exercise 1, add JJ closure for ex2

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var elements = document.getElementsByTagName("button");
 var n = elements.length;
+var i = 0;
 
-for (var i = 0; i < n; i++) {
+for (i = 0; i < n; i++) {
 	elements[i].onclick = function() {
 		alert(`this is ${i + 1}`);
 	};
